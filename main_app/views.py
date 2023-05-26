53d6b16df6702193e132de408acb7d4970a2039e
danielamansolino <danielamosqueragranda@gmail.com>
2023-05-26 13:37:43 -0400

    swshipcollector app create

---
+++ main_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+cats = [
+  {'name': 'Millennium Falcon', 'created_by': 'Corellian Engineering Corporation', 'class': 'Light freighter', 'maximum-speed': 1200},
+  {'name': 'X Wing', 'created_by': 'Incom Corporation', 'class': 'Starfighter', 'maximum-speed': 1050},
+  {'name': 'Imperial Star Destroyer', 'created_by': 'Kuat Drive Yards', 'class': 'Star Destroyer', 'maximum-speed':975},
+  
+]
+
+# Create your views here.
